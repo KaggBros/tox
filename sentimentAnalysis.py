7e2546654c8270f7e0af97c0f82e3c4dda4b0b37
Manemuneee <phash1989@gmail.com>
2017-12-25 14:59:41 +0100

    Some Descriptive Analysis

---
+++ sentimentAnalysis.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import nltk
+import csv
+import pandas as pd
+
+dfTrain = pd.from_csv('trina.csv')
+
+
+
+
+
+
